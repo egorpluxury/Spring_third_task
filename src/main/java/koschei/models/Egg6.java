586e097d7285bf87ecd29564dbd73b4e05200d59
egorpluxury <shevchenko.egor10@gmail.com>
2023-11-30 21:24:52 +0300

    Error fix

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,14 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    private Needle7 needle7;
+private final Needle7 needle7;
+@Autowired
+public Egg6(Needle7 needle7) {
+    this.needle7 = needle7;
+}
 
-  public Egg6(Needle7 needle7){
-      this.needle7=needle7;
-  }
-
-    @Override
-    public String toString() {
-        return ", в яйце иголка " + needle7.toString();
-    }
+@Override
+public String toString() {
+    return ", в яйце иголка " + needle7.toString();
+}
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
-private Deth8 deth8;
+    private Deth8 deth8;
     @Autowired
-    public void setDeth8(Deth8 deth8){
-    this.deth8=deth8;
-}
+    public void setDeth8(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
+
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + deth8.toString();
